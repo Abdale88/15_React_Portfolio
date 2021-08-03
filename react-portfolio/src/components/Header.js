6066adf43c5e34c5cfbcca287bf20e2524e99696
Abdale Hassan <cabdale989@gmail.com>
2021-08-03 12:07:26 -0500

    add a header

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import 'bulma/css/bulma.min.css';
+import dribble from '../images/dribbble_icon.png'
+import '../style/index.css'
 
 function Header() {
-  return (<div>Hello world</div>)
+  return (<nav className="clearfix">
+  <a className="is-size-3 has-text-black" href="#me">Abdale Hassan</a>
+  <a href="#"><img src={dribble} alt="dribbble icon"/></a>
+   </nav>
+)
 }
 
 export default Header;
