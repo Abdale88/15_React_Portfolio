e6c7796d76471dfdb1522b1208e646d3c1e8e446
Abdale Hassan <cabdale989@gmail.com>
2021-08-04 18:31:34 -0500

    import the screenshots

--- react-portfolio/src/components/Project.js
+++ react-portfolio/src/components/Project.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css';
 import '../style/index.css'
+import passwordGenerator from '../images/03-javascript-homework-demo.png'
+import project2 from '../images/project2.png'
+import schedule from '../images/schedule.gif'
+import Weather from '../images/Weather.png'
+import project1 from '../images/project1.png'
 
 function Project() {
   return (<div></div>)
