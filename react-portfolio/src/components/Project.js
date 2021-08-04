800fe3f606664a4b3a3203d214f47594486b3caf
Abdale Hassan <cabdale989@gmail.com>
2021-08-04 18:43:09 -0500

    add a project,link to the github, link to the deployed version and some bulma styling

--- react-portfolio/src/components/Project.js
+++ react-portfolio/src/components/Project.js
@@ -6,9 +6,28 @@ import project2 from '../images/project2.png'
 import schedule from '../images/schedule.gif'
 import Weather from '../images/Weather.png'
 import project1 from '../images/project1.png'
+import resume from '../images/Resume1.pdf'
 
 function Project() {
-  return (<div></div>)
+  return (<div>
+
+   <section id="projects" class="is-size-3"><h1>Projects</h1></section>
+    <div class="work">
+      <figure>
+        <h2 class="is-size-3 has-text-white">MyTeam</h2>
+        <a href="https://abc-myteam.herokuapp.com/login" target="_blank"><img src={project2} alt="Password Generator"/></a>
+        <div class="buttons is-centered">
+          <button class="button is-success is-outlined"><a href="https://abc-myteam.herokuapp.com/login" target="_blank">Click to View</a></button>
+          <button class="button is-success is-outlined"><a href="https://github.com/BTDubbzzz/athletics-admin" target="_blank">GitHub Link</a></button>
+       </div>
+      </figure>
+
+ 
+    </div>
+
+
+    
+  </div>)
 }
 
 export default Project;
